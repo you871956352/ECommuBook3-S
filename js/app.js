ecc233f4f95adfa2e0bb8db9f72455965c968fa6
you871956352 <you871956352@gmail.com>
2018-10-15 14:08:07 +0800

    Add status bar in ios

--- js/app.js
+++ js/app.js
@@ -24,7 +24,8 @@ angular
   .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
       if (window.StatusBar) {
-        //
+        StatusBar.styleDefault();
+        StatusBar.backgroundColorByHexString('#262424');
       }
     });
   })
